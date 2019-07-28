6b6aacc2cafbf609965d785a14f6eb187a6473d7
bxq2011hust <bxq2011hust@qq.com>
2019-07-28 12:22:14 +0800

    add travis ci

---
+++ .ci/ci_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -LO https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/master/tools/build_chain.sh && chmod u+x build_chain.sh
+bash build_chain.sh -l "127.0.0.1:4" -p 30300,20200,8545 -i
+bash nodes/127.0.0.1/start_all.sh
+cp nodes/127.0.0.1/sdk/* packages/cli/conf/
+cd packages/cli 
+./cli.js getClientVersion
